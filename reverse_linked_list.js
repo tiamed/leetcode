7d1fc9a6303740fb699ae6cee2c7886a32347155
tiamed <tiamedue@gmail.com>
2019-03-31 02:31:21 +0800

    3/31/2019

---
+++ reverse_linked_list.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let prev = null;
+    let next = null;
+    let cur = head;
+    while(cur !== null) {
+        next = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = next;
+    }
+    return prev;
+    
+};
+
